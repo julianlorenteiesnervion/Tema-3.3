a774134f3cdb63072348b0abd3464f93dbc54c10
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-01-15 09:16:51 +0100

    Ejercicio 3 NO terminado.

---
+++ src/boletincolecciones/Ejer3.java
@@ -0,0 +1,13 @@
+package boletincolecciones;
+
+import java.util.*;
+
+public class Ejer3 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>numbers = new ArrayList<>();
+		
+		
+	}
+
+}
