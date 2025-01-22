fd2e1badb62ba6bc52a0d5026affe23034116f74
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-01-22 08:44:09 +0100

    Ejercicio 3 terminado.

--- src/boletincolecciones/Ejer3.java
+++ src/boletincolecciones/Ejer3.java
@@ -5,8 +5,24 @@ import java.util.*;
 public class Ejer3 {
 
 	public static void main(String[] args) {
+		// Método random
+		Random random = new Random();
+		
+		// Creamos la lista de los números
 		ArrayList<Integer>numbers = new ArrayList<>();
 		
+		// Añadimos 30 veces un número aleatorio a la lista
+		for (int i = 0; i < 30; i++) {
+			numbers.add(random.nextInt(1, 11));
+		}
+		
+		// Ordenamos la lista
+		Collections.sort(numbers);
+		
+		// Recorremos la lista con un for each e imprimimos el valor
+		for (int value : numbers) {
+			System.out.print(value + " ");
+		}
 		
 	}
 
