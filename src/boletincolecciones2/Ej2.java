e8b3d5197c2c1d47a2f59faf77dcd0e3d803c9a8
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-01-28 13:39:00 +0100

    Merge branch 'master' of https://github.com/julianlorenteiesnervion/Tema-3.3.git

--- src/boletincolecciones2/Ej2.java
+++ src/boletincolecciones2/Ej2.java
@@ -7,7 +7,13 @@ public class Ej2 {
 	public static void main(String[] args) {
 		Random random = new Random();
 		
+		Set<Integer>randomNumbers = new LinkedHashSet<>();
 		
+		while (randomNumbers.size() < 10) {
+			randomNumbers.add(random.nextInt(1, 21));
+		}
+		
+		System.out.println(randomNumbers);
 		
 	}
 
