c62951329fb427d90bc8fd410ac3157d713aca57
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-01-31 14:28:59 +0100

    Ejercicio 6 NO terminado.

--- src/boletincolecciones2/Ejer6.java
+++ src/boletincolecciones2/Ejer6.java
@@ -16,26 +16,37 @@ public class Ejer6 {
 		
 		int phone;
 		
-		System.out.println("1. Añadir persona.\n"
-				+ "2. Añadir teléfono a una persona dada.\n"
-				+ "3. Mostrar los teléfonos de una persona.\n"
-				+ "4. Eliminar teléfono de una persona.\n"
-				+ "5. Eliminar una persona.");
-		
-		input = reader.nextInt();
-		
-		switch (input) {
-		case 1 -> {
-			System.out.print("Introduce el nombre: ");
-			name = reader.nextLine();
+		do {
+			System.out.println("1. Añadir persona.\n"
+					+ "2. Añadir teléfono a una persona dada.\n"
+					+ "3. Mostrar los teléfonos de una persona.\n"
+					+ "4. Eliminar teléfono de una persona.\n"
+					+ "5. Eliminar una persona.\n"
+					+ "0. Salir.");
 			
-			System.out.print("Introduce el teléfono: ");
-			phone = reader.nextInt();
+			input = reader.nextInt();
+			reader.nextLine();
 			
-			people.put(name, LinkedHashSet<phone>);
-		}
-		}
-		
+			switch (input) {
+			case 1 -> {
+				System.out.print("Introduce el nombre: ");
+				name = reader.nextLine();
+				
+				System.out.print("Introduce el teléfono: ");
+				phone = reader.nextInt();
+				
+				if (people.get(name) == null) {
+					people.put(name, new LinkedHashSet<>());
+				}
+				
+				if (people.get(name) != null) {
+					people.get(name).add(phone);
+				}
+				
+				System.out.println(people);
+			}
+			}
+		} while (input != 0);
 	}
 
 }
